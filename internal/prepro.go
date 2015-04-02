e319fcf3c1d4605a2364e6a8ffc6f801f341b187
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2015-04-03 00:49:36 +0200

    prepro.go: s/L/_L/ as workaround to avoid having it changed to nil

--- internal/prepro.go
+++ internal/prepro.go
@@ -6,9 +6,23 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"regexp"
 
 	"gopkg.in/pipe.v2"
 )
+
+func Replace(from, to string, regex bool) pipe.Pipe {
+	if !regex {
+		return pipe.Replace(func(line []byte) []byte {
+			return bytes.Replace(line, []byte(from), []byte(to), -1)
+		})
+	} else {
+		pattern := regexp.MustCompile(from)
+		return pipe.Replace(func(line []byte) []byte {
+			return pattern.ReplaceAll(line, []byte(to))
+		})
+	}
+}
 
 func main() {
 	err := pipe.Run(pipe.Line(
@@ -16,9 +30,14 @@ func main() {
 		pipe.Filter(func(line []byte) bool {
 			return !bytes.HasPrefix(line, []byte("#include "))
 		}),
-		pipe.Replace(func(line []byte) []byte {
-			return bytes.Replace(line, []byte("->top -="), []byte("->top += -"), -1)
-		}),
+		Replace("->top -=", "->top += -", false),
+		// FIXME(akavel): c2go chokes on func argument named L,
+		// replacing all occurences with 'nil' - below we workaround
+		Replace(`\bL\b`, `_L`, true),
+		Replace(`->_L\b`, `->L`, true),
+		Replace(`\._L\b`, `.L`, true),
+		Replace(`'_L'`, `'L'`, true),
+		Replace(`"_L"`, `"L"`, true),
 		pipe.Write(os.Stdout),
 	))
 	if err != nil {
